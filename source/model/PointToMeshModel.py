e637ced576bd897e1a501fd4b6100db9a9b989fa
David Charatan <david.charatan@gmail.com>
2020-11-28 00:59:09 -0800

    Add final convolution to model.

--- source/model/PointToMeshModel.py
+++ source/model/PointToMeshModel.py
@@ -1,6 +1,7 @@
 from tensorflow.keras import Model
 from ..layers.composite.Encoder import Encoder
 from ..layers.composite.Decoder import Decoder
+from ..layers.convolution.MeshConvolution import MeshConvolution
 from ..mesh.Mesh import Mesh
 import tensorflow as tf
 
@@ -8,13 +9,16 @@ import tensorflow as tf
 class PointToMeshModel(Model):
     def __init__(self) -> None:
         super(PointToMeshModel, self).__init__()
-        self.encoder = Encoder((12, 24, 48), 3, 4, 0.1, (27, 24, None))
-        self.decoder = Decoder((24, 12, 6), 3, 4, 0.1, (27, 24, None))
+        self.encoder = Encoder((16, 32, 64), 1, 1, 0.1, (None, None, None))
+        self.decoder = Decoder((32, 16, 6), 1, 1, 0.1, (None, None, None))
+        initializer = tf.keras.initializers.RandomUniform(-1e-8, 1e-8)
+        self.final_convolution = MeshConvolution(6, initializer, initializer)
 
     def call(self, mesh: Mesh, fixed_input_features: tf.Tensor):
         assert tf.rank(fixed_input_features) == 2 and fixed_input_features.shape[1] == 6
 
         encoding, snapshots = self.encoder(mesh, fixed_input_features)
-        output = self.decoder(mesh, encoding, snapshots)
+        decoding = self.decoder(mesh, encoding, snapshots)
+        output = self.final_convolution(mesh, decoding)
 
         return output
