dc12399b5a8671f195d0d56e960f224f13842fce
Solon James <solon_james@brown.edu>
2020-12-04 18:42:02 -0500

    changed train model to hceck for stagnating loss

--- source/script_modules/train_model.py
+++ source/script_modules/train_model.py
@@ -78,49 +78,77 @@ for subdivision_level in range(num_subdivisions):
 
     old_vertices = tf.convert_to_tensor(remeshed_vertices, dtype=tf.float32)
     num_iterations = options["num_iterations"]
+
+    chamfer_losses = np.ones(100)
+    chamfer_loss_iterator = 0
+    chamfer_loss_terminator = False
+    beamgap_losses = np.ones(25)
+    beamgap_loss_iterator = 0
+    beamgap_loss_terminator = False
+
     for iteration in range(num_iterations):
         iteration_start_time = time.time()
+        if (not beamgap_loss_terminator) or (not chamfer_loss_terminator):
+            with tf.GradientTape() as tape:
+                # Get new vertex positions by calling the model.
+                features = model(mesh, in_features)
+                new_vertices = old_vertices + get_vertex_features(mesh, features)
 
-        with tf.GradientTape() as tape:
-            # Get new vertex positions by calling the model.
-            features = model(mesh, in_features)
-            new_vertices = old_vertices + get_vertex_features(mesh, features)
+                # Calculate loss.
+                surface_sample = mesh.sample_surface(new_vertices, 10000)
+                beamgap_modulo = options["beamgap_modulo"]
+                if beamgap_modulo == -1:
+                    use_beamgap_loss = False
+                else:
+                    use_beamgap_loss = iteration % beamgap_modulo == 0
+                if use_beamgap_loss:
+                    beam_loss.update_points_masks(mesh, new_vertices, point_cloud_tf)
+                    total_loss = 0.01 * beam_loss(mesh, new_vertices)
+                    beamgap_losses[beamgap_loss_iterator] = total_loss.numpy.item()
+                    beamgap_loss_iterator = (beamgap_loss_iterator + 1) % 25
+                    beamgap_loss_terminator = (
+                        np.abs(
+                            total_loss.numpy.item()
+                            - beamgap_losses[beamgap_loss_iterator]
+                        )
+                        / beamgap_losses[beamgap_loss_iterator]
+                        < 0.01
+                    )
+                else:
+                    total_loss = chamfer_loss(surface_sample[0], point_cloud_tf)
+                    chamfer_losses[chamfer_loss_iterator] = total_loss.numpy.item()
+                    chamfer_loss_iterator = (chamfer_loss_iterator + 1) % 100
+                    chamfer_loss_terminator = (
+                        np.abs(
+                            total_loss.numpy.item()
+                            - chamfer_losses[chamfer_loss_iterator]
+                        )
+                        / chamfer_losses[chamfer_loss_iterator]
+                        < 0.01
+                    )
 
-            # Calculate loss.
-            surface_sample = mesh.sample_surface(new_vertices, 10000)
-            beamgap_modulo = options["beamgap_modulo"]
-            if beamgap_modulo == -1:
-                use_beamgap_loss = False
-            else:
-                use_beamgap_loss = iteration % beamgap_modulo == 0
-            if use_beamgap_loss:
-                beam_loss.update_points_masks(mesh, new_vertices, point_cloud_tf)
-                total_loss = 0.01 * beam_loss(mesh, new_vertices)
-            else:
-                total_loss = chamfer_loss(surface_sample[0], point_cloud_tf)
+            # Apply gradients.
+            gradients = tape.gradient(total_loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-        # Apply gradients.
-        gradients = tape.gradient(total_loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+            # Save the obj every few iterations.
+            save_modulo = options["obj_save_modulo"]
+            if iteration % save_modulo == 0:
+                save_mesh(
+                    f"tmp_{str(subdivision_level).zfill(2)}_{str(iteration).zfill(3)}.obj",
+                    new_vertices.numpy(),
+                    remeshed_faces,
+                )
 
-        # Save the obj every few iterations.
-        save_modulo = options["obj_save_modulo"]
-        if iteration % save_modulo == 0:
-            save_mesh(
-                f"tmp_{str(subdivision_level).zfill(2)}_{str(iteration).zfill(3)}.obj",
-                new_vertices.numpy(),
-                remeshed_faces,
-            )
-
-        # Log a progress update.
-        loss_type = "Beam-gap" if use_beamgap_loss else "Chamfer"
-        message = [
-            f"{Back.WHITE}{Fore.BLACK}"
-            f" {subdivision_level + 1}/{num_subdivisions} & {iteration + 1}/{num_iterations}",
-            f"{Style.RESET_ALL}",
-            f"{loss_type} Loss: {total_loss.numpy().item()},",
-            f"Time: {time.time() - iteration_start_time}",
-        ]
-        print(" ".join(message))
+            # Log a progress update.
+            loss_type = "Beam-gap" if use_beamgap_loss else "Chamfer"
+            message = [
+                f"{Back.WHITE}{Fore.BLACK}"
+                f" {subdivision_level + 1}/{num_subdivisions} & {iteration + 1}/{num_iterations}",
+                f"{Style.RESET_ALL}",
+                f"{loss_type} Loss: {total_loss.numpy().item()},",
+                f"Time: {time.time() - iteration_start_time}",
+            ]
+            print(" ".join(message))
 
 print("Done.")
